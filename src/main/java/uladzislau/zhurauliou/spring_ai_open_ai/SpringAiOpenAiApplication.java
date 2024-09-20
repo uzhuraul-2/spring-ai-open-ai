24224acf5ae78945a6dbbee21fc4db228b678020
uzhuraul <uladzislau.zhurauliou@gmail.com>
2024-09-20 13:01:54 +0200

    Init

---
+++ src/main/java/uladzislau/zhurauliou/spring_ai_open_ai/SpringAiOpenAiApplication.java
@@ -0,0 +1,13 @@
+package uladzislau.zhurauliou.spring_ai_open_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOpenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOpenAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uladzislau/zhurauliou/spring_ai_open_ai/SpringAiOpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package uladzislau.zhurauliou.spring_ai_open_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiOpenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
